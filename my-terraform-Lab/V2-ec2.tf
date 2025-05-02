5ba6f1a562323d5de68327f44fd4d0e1f54fd2c9
rbansod <rbansod625@gmail.com>
2025-05-02 12:11:45 +0530

    v2 of file2

--- my-terraform-Lab/V2-ec2.tf
+++ my-terraform-Lab/V2-ec2.tf
@@ -2,21 +2,31 @@ provider "aws" {
 region  = "ap-south-1"
 }
 
-resource "aws_instance" "demo-server" {
-    ami = "ami-0f1dcc636b69a6438"
-    instance_type = "t2.micro"
-    key_name = "dpp"
-    security_groups = "demo-sg"
-  
-}
-
 resource "aws_security_group" "demo-sg" {
   name        = "demo-sg"
   description = "Allow TLS inbound traffic and all outbound traffic"
 
+
+   ingress {
+     description = "HTTPS"
+     from_port   = 443
+     to_port     = 443
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+
+   ingress {
+     description = "HTTP"
+     from_port   = 80
+     to_port     = 80
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+
+
   ingress {
     description = "SSH access"
-    protocol  = tcp
+    protocol  = "tcp"
     from_port = 22
     to_port   = 22
     cidr_blocks = ["0.0.0.0/0"]
@@ -32,4 +42,13 @@ resource "aws_security_group" "demo-sg" {
   tags = { 
     name = "ssh-port"
   }
+}
+
+resource "aws_instance" "demo-server" {
+    ami = "ami-062f0cc54dbfd8ef1"
+    instance_type = "t2.micro"
+    key_name = "dpp"
+    security_groups = [ "demo-sg" ]
+
+
 }
